727426f3c583f9319af39b9604e320a9658237bf
Alex Nguyen <alex.nguyen@td.com>
2016-11-11 18:41:22 -0500

    Finished app page header

---
+++ Investment Hero/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Investment Hero
+//
+//  Created by Alex Nguyen on 2016-11-11.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let COLOUR_SCHEME = UIColor(red:0.38, green:0.75, blue:0.68, alpha:1.0);
