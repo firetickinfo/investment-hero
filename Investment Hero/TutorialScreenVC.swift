7e95851069b7e453195adfe954ffadcb3394f062
Alex Nguyen <alex.nguyen@td.com>
2016-11-12 11:46:29 -0500

    Added welcome page

---
+++ Investment Hero/TutorialScreenVC.swift
@@ -0,0 +1,27 @@
+//
+//  TutorialScreenVC.swift
+//  Investment Hero
+//
+//  Created by Alex Nguyen on 2016-11-12.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class TutorialScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
---
+++ Investment Hero/WelcomePageVC.swift
@@ -0,0 +1,89 @@
+//
+//  WelcomePageVC.swift
+//  Investment Hero
+//
+//  Created by Alex Nguyen on 2016-11-12.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class WelcomePageVC: UIPageViewController, UIScrollViewDelegate {
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController(page: "page1"),
+                self.newColoredViewController(page: "page2"),
+                self.newColoredViewController(page: "page3")]
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+        self.view.backgroundColor = COLOUR_SCHEME
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    private func newColoredViewController(page: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(page)ViewController")
+    }
+}
+
+extension WelcomePageVC : UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        print(viewControllerIndex)
+        return orderedViewControllers[nextIndex]
+    }
+}
