6437ba39c905d9d28aea6608d8369d132c89f63e
Alex Nguyen <alex.nguyen@td.com>
2016-11-11 19:19:16 -0500

    configured firebase

---
+++ Investment Hero/DataService.swift
@@ -0,0 +1,15 @@
+//
+//  DataService.swift
+//  Investment Hero
+//
+//  Created by Alex Nguyen on 2016-11-11.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    //Singleton
+    static let shared = DataService()
+    
+}
